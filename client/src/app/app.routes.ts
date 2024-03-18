3cdcf72126889c6ddaf0582d7cb5d44499312e68
CsalaPeter <csalapeter95@gmail.com>
2024-03-18 14:53:22 +0100

    Added user model and basic login component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { RecipeComponent } from './view/recipe/recipe.component';
 import { SearchViewComponent } from './view/search-view/search-view.component';
 import { NewRecipeComponent } from './view/new-recipe/new-recipe.component';
+import { RegisterComponent } from './account/register/register.component';
+import { LoginComponent } from './account/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'recipe/:id', component: RecipeComponent },
   { path: 'search/:value', component: SearchViewComponent },
   { path: 'new-recipe', component: NewRecipeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
 ];
